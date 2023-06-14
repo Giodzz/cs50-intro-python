598c59354772cf71477bf8d6b18b42d6c328b11a
Giodzz <giordanabbucci@gmail.com>
2023-06-13 22:50:36 -0300

    feat: pede um input e o utiliza em um print formatado

--- lecture0/hello.py
+++ lecture0/hello.py
@@ -1,4 +1,7 @@
-print("Hello, World!")
+name = input("What's your name? ") # function with return values
+# variable is a container for some value inside the computer
+
+print(f"Hello, {name}!") # formatted string
 
 # print() is a function in python
 # what are inside the parenthesis are arguments
