138ba36450a3083c9738e9a20845a104d3bb780c
Giodzz <giordanabbucci@gmail.com>
2023-06-15 17:41:01 -0300

    Utilizando os keyarguments da função print

--- lecture0/hello.py
+++ lecture0/hello.py
@@ -1,7 +1,14 @@
 name = input("What's your name? ") # function with return values
 # variable is a container for some value inside the computer
 
-print(f"Hello, {name}!") # formatted string
 
+print("Hello,", end=' ')
+print(name)
 # print() is a function in python
 # what are inside the parenthesis are arguments
+
+
+# another way to print
+print(f"Hello, {name}!") # formatted string
+
+
