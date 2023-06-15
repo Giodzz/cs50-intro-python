f537b2ad7503af911f0a0ea1212c97b4e871cfd1
Giodzz <giordanabbucci@gmail.com>
2023-06-15 17:51:33 -0300

    feat: método capitalize

--- lecture0/testingStrings.py
+++ lecture0/testingStrings.py
@@ -4,5 +4,8 @@ name = input("What's your name? ")
 # Remove whitespace from str (begginig and end)
 name = name.strip() 
 
+#Capitalize user's name
+name = name.capitalize()
+
 #Say hello to user
 print(f"Hello, {name}!")
